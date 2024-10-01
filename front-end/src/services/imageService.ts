2d309ba6e7dc0073bae8d64e9d36176a0c404ac2
Mestas <matiasolmosrivero@gmail.com>
2024-10-01 20:19:19 -0300

    Cagada con git xd

--- front-end/src/services/imageService.ts
+++ front-end/src/services/imageService.ts
@@ -10,11 +10,8 @@ export const uploadImages = async (images: File[]) => {
 
   try {
     // Realizamos la petición POST para subir las imágenes
-<<<<<<< HEAD
     const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/images/upload`, formData, {
-=======
-    const response = await axios.post(`${process.env.RREACT_APP_API_BASE_URL}/api/images/upload`, formData, {
->>>>>>> 1c0c06b9c65b30931ddceed5161654579258f60e
+    
       headers: {
         'Content-Type': 'multipart/form-data',
       },
@@ -31,11 +28,7 @@ export const getImageByID = async (id : string) => {
   
   try {
     // Realizamos la petición POST para subir las imágenes
-<<<<<<< HEAD
     const response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/images/${id}`);
-=======
-    const response = await axios.get(`${process.env.RREACT_APP_API_BASE_URL}/api/images/${id}`);
->>>>>>> 1c0c06b9c65b30931ddceed5161654579258f60e
 
     return response.data;
   } catch (error) {
